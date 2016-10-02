30ec336c0f1949b1f2bf7595027a700de8626b83
shaquib-nesar <nesar.shaquib@gmail.com>
2016-10-02 08:14:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName', function(req,res){
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
