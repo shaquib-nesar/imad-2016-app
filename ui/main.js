21196e863d483b477b6a69c06d796a8103d54dd6
shaquib-nesar <nesar.shaquib@gmail.com>
2016-10-03 17:45:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = "Welcome";
 
 //mov the image
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight() {
+     marginLeft = marginLeft + 10;
+     img.style.marginLeft = marginLeft + "px";
+}
 img.onclick = function(){
-    img.style.marginLeft = "100px";
+    var interval = setInterval(moveRight, 100);
 };
