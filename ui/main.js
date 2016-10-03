321084a7f2c72ddc06ad5eaed42c122332c69069
shaquib-nesar <nesar.shaquib@gmail.com>
2016-10-03 17:57:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,16 @@ function moveRight() {
      marginLeft = marginLeft + 1;
      img.style.marginLeft = marginLeft + "px";
 }
+
+//from here
+var marginDown = 0;
+function moveUp(){
+    marginDown = marginDown + 1;
+    img.style.marginDown = marginDown + "px";
+}
+
+//upto here
 img.onclick = function(){
     var interval = setInterval(moveRight, 10);
+    var interval2 = setInterval(moveUp, 10);  //this line
 };
