be72dc82dc17645f2b742cc2eed4307bd723ff81
shaquib-nesar <nesar.shaquib@gmail.com>
2016-10-03 18:03:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,19 +8,11 @@ element.innerHTML = "Welcome";
 var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight() {
-     marginLeft = marginLeft + 50;
+     marginLeft = marginLeft - 5;
      img.style.marginLeft = marginLeft + "px";
 }
 
-//from here
-var marginDown = 0;
-function moveUp(){
-    marginDown = marginDown - 50;
-    img.style.marginDown = marginDown + "px";
-}
 
-//upto here
 img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
-    var interval2 = setInterval2(moveUp, 100);  //this line
+    var interval = setInterval(moveRight, 10);
 };
