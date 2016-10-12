49851d86863b537e81530e48c5ed5b2c9ca16213
shaquib-nesar <nesar.shaquib@gmail.com>
2016-10-12 10:18:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //change the text of main-text div
 var element = document.getElementById("main-text");
-element.innerHTML = "Welco";
+element.innerHTML = "Welcome";
 
 //mov the image
 var img = document.getElementById("madi");
